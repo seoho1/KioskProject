edcbd5e6f758a898748c14aeeef7a7fd2701f8f3
flashback7 <97232474+seoho1@users.noreply.github.com>
2024-11-25 20:48:09 +0900

    Lv2 menuItem class, Implemented list related to menuItem

--- src/com/example/kiosk/MenuItem.java
+++ src/com/example/kiosk/MenuItem.java
@@ -1,4 +1,25 @@
 package com.example.kiosk;
 
 public class MenuItem {
+    private final String burgerName;
+    private final double price;
+    private final String description;
+
+    public MenuItem(String burgerName, double price, String description) {
+        this.burgerName = burgerName;
+        this.price = price;
+        this.description = description;
+    }
+
+    public String getBurgerName() {
+        return burgerName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
